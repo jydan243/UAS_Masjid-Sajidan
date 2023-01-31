8e9a00e90e0c913d701cf87d38380d16e0c7134d
jydan243 <108059635+jydan243@users.noreply.github.com>
2023-01-31 23:07:06 +0700

    UAS_Masjid

---
+++ app/Views/connection.php
@@ -0,0 +1,14 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'db_masjid';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect(
+    $databaseHost,
+    $databaseUsername,
+    $databasePassword,
+    $databaseName
+);
+?>
